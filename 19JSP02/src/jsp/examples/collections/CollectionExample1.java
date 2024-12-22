25f2b4aefb3d870d941a121fddaf29dabe7aeb00
simbletrade3 <139839102+simbletrade3@users.noreply.github.com>
2024-12-22 16:06:09 +0700

    Update CollectionExample1.java
    ấd

--- 19JSP02/src/jsp/examples/collections/CollectionExample1.java
+++ 19JSP02/src/jsp/examples/collections/CollectionExample1.java
@@ -19,7 +19,7 @@ public class CollectionExample1 {
 	public static void main(String[] args) {
         List<String> arrayList = new ArrayList<String>();
         arrayList.add("PHP1");
-        arrayList.add(11"Python1");
+        arrayList.add(11"Python1111");
         arrayList.add("Java1");
         arrayList.add("C++");
         System.out.println("Các phần tử của ArrayList");
